4f31739513b0473790efc2bae71045c1cfa1f864
linlin <952622946@qq.com>
2020-02-08 03:05:56 +0800

    fix(dockerfile): 优化上线流程

--- app/graphql/schema/userResolver.ts
+++ app/graphql/schema/userResolver.ts
@@ -6,14 +6,13 @@ import { Resolver, Query, Mutation } from 'type-graphql';
 @Resolver(User)
 export class UserResolver {
   @Query(() => [User], { description: '获取所有用户列表' })
-  async getUser() {
+  async users() {
     return await UserModel.findUserList();
   }
 
   @Mutation(() => User, { description: '增加用户 ' })
-  async addUser() {
-    return await UserModel.createUser();
-    // return await UserService.add();
+  async addUser(@Ctx() ctx: Context) {
+    return await ctx.service.user.add();
   }
 
   @Mutation(() => User, { description: '通过service来增加用户' })
